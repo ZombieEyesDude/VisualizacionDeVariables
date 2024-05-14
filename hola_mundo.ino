9cf3cc14cdd0a4cfda6fa2e9808976e6df10feaa
Ian Acevedo <albornoztobias92@gmail.comgit config --global user.name ZimbieEyesDude>
2024-05-14 19:16:18 -0300

    test

---
+++ hola_mundo.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// Dirección I2C del display
+#define I2C_ADDR 0x27
+
+// Número de columnas y filas del display
+#define LCD_COLUMNS 16
+#define LCD_ROWS 2
+
+// Inicializamos el objeto LCD
+LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_ROWS);
+
+void setup() {
+  // Inicializamos el LCD
+  lcd.init();
+
+  // Encendemos el backlight
+  lcd.backlight();
+
+  // Imprimimos "Hola Mundo" en la primera fila
+  lcd.setCursor(0, 0);
+  lcd.print("Hola Mundo!");
+}
+
+void loop() {
+  // El loop está vacío ya que no necesitamos realizar ninguna tarea adicional
+}
